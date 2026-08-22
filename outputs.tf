81161ecc4eaece4a00e6eee284f54c048fc28a6f
Daniel Allen <dan@aeternagroup.net>
2026-08-22 23:36:36 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "iam_role_policies_exclusives_id" {
-  description = "Map of id values across all iam_role_policies_exclusives, keyed the same as var.iam_role_policies_exclusives"
-  value       = { for k, v in aws_iam_role_policies_exclusive.iam_role_policies_exclusives : k => v.id if v.id != null && length(v.id) > 0 }
-}
 output "iam_role_policies_exclusives_policy_names" {
   description = "Map of policy_names values across all iam_role_policies_exclusives, keyed the same as var.iam_role_policies_exclusives"
   value       = { for k, v in aws_iam_role_policies_exclusive.iam_role_policies_exclusives : k => v.policy_names if v.policy_names != null && length(v.policy_names) > 0 }
